dfaf6b6e7fc71772946ebc94fa5f89aa220f438c
Pawel Kowaliszyn <pkowaliszyn@onet.eu>
2017-12-07 20:21:31 +0100

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 var span = $('span');
+var paragraphs = $('p');
 
 span.each(function(index, element) {
     if(index % 2 == 0) {
         $(element).css('color', 'red');
-    };
+    }
 });
-
-var paragraphs = $('p');
 
 paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
-
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    $(element).append(button);
 });
 
-$("button").click(function(){
-    alert($(this).attr("data-tmp"));
+$('button').click(function(){
+    alert($(this).attr('data-tmp'));
 });
